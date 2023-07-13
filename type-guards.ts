b424e9eb1d64d53bef82cc55dbd860872d4fbe39
Hanna Nyman <forename.surname@viaplaygroup.com>
2023-07-13 12:15:56 +0200

    adding comments for explanation

--- type-guards.ts
+++ type-guards.ts
@@ -1,4 +1,8 @@
+// type guards performs a run-time type check, since TypeScript only exists in compile-time
+
 type alphanumeric = string | number;
+
+// check if both a and b are of string type
 
 function add(a: alphanumeric, b: alphanumeric) {
     if (typeof a === 'string' && typeof b === 'string') {
